9ebca48e58050051d894f412c7d75093d6e234e9
andrius444 <andrius.sareika@gmail.com>
2017-02-24 19:06:24 +0200

    #53 - basic GeoSuggest implemented

--- app/js/config/Tester.js
+++ app/js/config/Tester.js
@@ -1,17 +1,40 @@
 var React = require('react');
+var Geosuggest = require('react-geosuggest').default;
 
 var Tester = React.createClass({
     getInitialState: function() {
-        return ({ name: "" });
+        return ({ address: "" });
     },
-    renderEdit: function(e) {
-        console.log(e);
+    onSuggestSelect: function(suggest) {
+        this.setState({ address: suggest.label });
+    },
+    hideOtherSuggestions: function() {
+        $('.geosuggest__suggests').hide();
+    },
+    clearSuggest: function() {
+        this._geoSuggest.clear();
+        this.setState({ address: "" });
     },
     render: function() {
         return (
             <div>
-                <p id="vardas">Vardas</p>
-                <button onClick={this.renderEdit}>Edit</button>
+                <p>Address selected:</p>
+                <p id="vardas">{this.state.address}</p>
+                <hr />
+                <br />
+
+                <Geosuggest
+                    ref={el=>this._geoSuggest=el}
+                    placeholder="Start typing!"
+                    className="class-name-GEO"
+                    inputClassName="input-class-name-GEO"
+                    country="lt"
+                    queryDelay='500'
+                    onSuggestSelect={this.onSuggestSelect}
+                    onActiveSuggest={this.hideOtherSuggestions}
+                />
+
+                <button onClick={this.clearSuggest}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
             </div>
         );
     }
