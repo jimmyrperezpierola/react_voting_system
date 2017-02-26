1403a1a5d197f139eb736d889078b8db557d983f
andrius444 <andrius.sareika@gmail.com>
2017-02-26 15:51:03 +0200

    #58 -

--- app/js/config/Tester.js
+++ app/js/config/Tester.js
@@ -1,45 +1,26 @@
 var React = require('react');
-var Geosuggest = require('react-geosuggest').default;
+var axios = require('axios');
+var fileDownload = require('react-file-download');
 
 var Tester = React.createClass({
-    getInitialState: function() {
-        return ({ address: "" });
-    },
-    onSuggestSelect: function(suggest) {
-        this.setState({ address: suggest.label });
-    },
-    hideOtherSuggestions: function() {
-        $('.geosuggest__suggests').hide();
-    },
-    clearSuggest: function() {
-        this._geoSuggest.blur();
-        this.setState({ address: "" });
-    },
-    changeAddress: function(value) {
-        this.setState({ address: value });
+    downloadCSV() {
+        axios.get('http://localhost:8080/api/download/testers')
+             .then(resp => {
+                console.log(resp);
+                fileDownload(resp.data, "testData.csv");
+             })
+             .catch(err => {
+                console.log(err);
+             });
     },
     render: function() {
-        console.log(this.state.address);
         return (
             <div>
-                <p>Address selected:</p>
-                <p id="vardas">{this.state.address}</p>
-                <hr />
-                <br />
-
-                <Geosuggest
-                    ref={el=>this._geoSuggest=el}
-                    placeholder="Start typing!"
-                    className="class-name-GEO"
-                    inputClassName="input-class-name-GEO"
-                    country="lt"
-                    queryDelay='500'
-                    onSuggestSelect={this.onSuggestSelect}
-                    onActiveSuggest={this.hideOtherSuggestions}
-                    onChange={this.changeAddress}
-                />
-
-                <button onClick={this.clearSuggest}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+                <button
+                    onClick={this.downloadCSV}>
+                    <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>&nbsp;
+                    Download
+                </button>
             </div>
         );
     }
