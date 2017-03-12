a4bdb2625239165faf4397c3b965434e97a41d54
Osvaldas <o.rimkus@gmail.com>
2017-03-12 13:26:36 +0200

    minor fix in candidate info tab inside search page, also ids for testing purposes added

--- app/js/search/SearchCandidateCardComponent.js
+++ app/js/search/SearchCandidateCardComponent.js
@@ -5,14 +5,14 @@ var styles = {"image": {width: 20, height: 20}, "marginLeft": {marginLeft: 25}}
 var partyName;
 
 function SearchCandidateCardComponent(props) {
-  partyName = (props.candidate.partyName == undefined) ? "Išsikėlęs pats" : props.candidate.partyName;
+  partyName = (props.candidate.party == null) ? "Išsikėlęs pats" : props.candidate.party.name;
   return (
         <div className="well well-sm candidate-card-wide">
                 <div className="row candidate-card-low">
                     <div className="col-sm-8 col-md-12" style={{backgroundColor: "white"}}>
                         <p style={{ marginTop: 15 }}>
-                            <h4 style={ styles.marginLeft }>
-                                <span>
+                            <p style={ styles.marginLeft, {fontSize: "22"} }>
+                                <span id={"candidate-info-tab-name-" + props.candidate.personId} >
                                     <span style={{color:'#1c9312', fontWeight:"bold"}}>
                                         {props.candidate.firstName} &nbsp; {props.candidate.lastName}
                                     </span>
@@ -23,19 +23,19 @@ function SearchCandidateCardComponent(props) {
                                     style={{ float: 'right', cursor: 'pointer' }}
                                     onClick={props.hideDetails}>
                                 </span>
-                            </h4>
+                            </p>
                         </p>
                         <p>
-                            <small><img src="app/imgs/political_party.png" style={ styles.image }/>&nbsp;&nbsp;<b>Partija:</b>&nbsp; {partyName}</small>
+                            <small id={"candidate-info-tab-party-dependency-" + props.candidate.personId} ><img src="app/imgs/political_party.png" style={ styles.image }/>&nbsp;&nbsp;<b>Partija:</b>&nbsp; {partyName}</small>
                         </p>
                         <p>
-                            <small><img src="app/imgs/fingerprint.png" style={ styles.image }/>&nbsp;&nbsp;<b>A/K:</b>&nbsp; {props.candidate.personId}</small>
+                            <small id={"candidate-info-tab-personal-id-" + props.candidate.personId} ><img src="app/imgs/fingerprint.png" style={ styles.image }/>&nbsp;&nbsp;<b>A/K:</b>&nbsp; {props.candidate.personId}</small>
                         </p>
                         <p>
-                            <small><img src="app/imgs/cupcake.png" style={ styles.image }/>&nbsp;&nbsp;<b>Gimimo data:</b>&nbsp; {props.candidate.birthDate}</small>
+                            <small id={"candidate-info-tab-birth-day-" + props.candidate.personId} ><img src="app/imgs/cupcake.png" style={ styles.image }/>&nbsp;&nbsp;<b>Gimimo data:</b>&nbsp; {props.candidate.birthDate}</small>
                         </p>
                         <p>
-                            <small>
+                            <small id={"candidate-info-tab-information-" + props.candidate.personId} >
                                 <img src="app/imgs/books.png" style={ styles.image }/>&nbsp;&nbsp;
                                 <b>Apie:</b>&nbsp;<br />
                                 <span style={ styles.marginLeft }>{props.candidate.description}</span>
@@ -45,14 +45,14 @@ function SearchCandidateCardComponent(props) {
                         <p>
                             <small>
                                 <b style={{color: "red"}}>/funkcionalumo nėra/</b><br />
-                                Užimta vieta vienmandačiuose - 10<br />
-                                <b>Išrinktas</b>
+                                Užimta vieta vienmandačiuose - <span id={"candidate-info-tab-single-mandate-position-" + props.candidate.personId} >10</span><br />
+                                <b id={"candidate-info-tab-single-elected-" + props.candidate.personId}>Išrinktas</b>
                             </small>
                         </p>
                         <p>
                             <small>
-                                Užimta vieta daugiamandačiuose - 10<br />
-                                <b>Neišrinktas</b>
+                                Užimta vieta daugiamandačiuose - <span id={"candidate-info-tab-multi-mandate-position-" + props.candidate.personId} >10</span><br />
+                                <b id={"candidate-info-tab-multi-elected-" + props.candidate.personId}>Neišrinktas</b>
                             </small>
                         </p>
                     </div>
