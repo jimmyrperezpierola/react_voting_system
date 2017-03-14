0ca28083bfda1a2bb803d540443adaab99bf50c5
OsvaldasRimkus <o.rimkus@gmail.com>
2017-03-14 20:45:30 +0200

    minor fix for Confirm Action component: fixed button text for clear usage

--- app/js/public_views/DistrictSMresultView.js
+++ app/js/public_views/DistrictSMresultView.js
@@ -229,21 +229,8 @@ var DistrictSMresultView = React.createClass({
         } else {
             return array;
         }
-<<<<<<< HEAD
-=======
-        array.push(max);
-
-        return Array.from(new Set(array)).sort((a, b) => {
-            // TODO perdaryti ĄČĘ rūšiavimą
-            if (a > b) {
-                return 1;
-            } else if (a < b) {
-                return -1;
-            }
-            return 0;
-        });
->>>>>>> 0acadf4593ee548c0022c7642fdabaa8a8b88d34
     },
+
     render() {
         return (
             <div>
